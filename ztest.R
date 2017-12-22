712b30f36f2e5d4a5d05f8d6cb9e266a8e3952ff
unknown <serafini@rcb5201-8.psyc.sfu.ca>
2017-12-22 15:17:58 -0800

    Add functions

--- ztest.R
+++ ztest.R
@@ -1,20 +1,26 @@
-data <- matrix(c( 1, .1, .2,
-                 .1,  1, .3,
-		         .2, .3,  1), nrow=3, ncol=3)
-				 
+z <- dget("fisherz.R")
+makesymmetric <- dget("MakeSymmetricMatrix.R")
+makedelta <- dget("MakeDeltaFromHypothesis.R")
+
+data <- matrix(c(
+1,0.08,0.03,
+0.08,1,0.11,
+0.03,0.11,1), nrow=3, ncol=3)
+
 hypothesis <- matrix(c(1, 1, 1,
                        2, 3, 3,
-					   1, 1, 2,
-					   1, 1, 1,
-					   0, 0, 0), nrow=3, ncol=5)
-					   
-delta <- matrix(c(1, 1, 1), nrow=3, ncol=1)
-
-z <- function(r) { return(0.5 * (log(1+r) - log(1-r)))
-				 }
-
+          					   1, 1, 2,
+          					   1, 1, 1,
+          					   0, 0, 0), nrow=3, ncol=5)
 
 N = 50
+k <- nrow(delta)
+q <- ncol(delta)
+
+
+
+					   
+delta <- makedelta(hypothesis)	   
 
 rows <- nrow(hypothesis)
 correlations <- c(0)
@@ -27,7 +33,6 @@ for (jj in 1:rows) {
 rhostar <- hypothesis[,5]
 gammaLS <- solve(t(delta)%*%delta)%*%t(delta)%*%(correlations - rhostar)
 rhoLS <- delta%*%gammaLS + rhostar
-
 Rlist <- data
 
 rows <- nrow(hypothesis)
@@ -37,6 +42,8 @@ for (jj in 1:rows) {
 	Rlist[j,k] <- rhoLS[jj]
 }
 
+Rlist <- makesymmetric(Rlist)
+
 Psi <- matrix(0, nrow=rows, ncol=rows)
 for (jj in 1:rows) {
     for (kk in 1:jj) {
@@ -45,26 +52,19 @@ for (jj in 1:rows) {
         h <- hypothesis[kk,2]
         m <- hypothesis[kk,3]
 
-		if (a != b) {
-			cov1 <- 0
-		} else {
-		
-			term1 <- ((Rlist[j,h] - Rlist[j,k]*Rlist[k,h])*(Rlist[k,m] - Rlist[k,h]*Rlist[h,m]))
-			term2 <- ((Rlist[j,m] - Rlist[j,h]*Rlist[h,m])*(Rlist[k,h] - Rlist[k,j]*Rlist[j,h]))
-			term3 <- ((Rlist[j,h] - Rlist[j,m]*Rlist[m,h])*(Rlist[k,m] - Rlist[k,j]*Rlist[j,m]))
-			term4 <- ((Rlist[j,m] - Rlist[j,k]*Rlist[k,m])*(Rlist[k,h] - Rlist[k,m]*Rlist[m,h]))
-			Psi[[jj,kk]] <- 0.5*(term1 + term2 + term3 + term4)
-		}
+  			term1 <- ((Rlist[j,h] - Rlist[j,k]*Rlist[k,h])*(Rlist[k,m] - Rlist[k,h]*Rlist[h,m]))
+  			term2 <- ((Rlist[j,m] - Rlist[j,h]*Rlist[h,m])*(Rlist[k,h] - Rlist[k,j]*Rlist[j,h]))
+  			term3 <- ((Rlist[j,h] - Rlist[j,m]*Rlist[m,h])*(Rlist[k,m] - Rlist[k,j]*Rlist[j,m]))
+  			term4 <- ((Rlist[j,m] - Rlist[j,k]*Rlist[k,m])*(Rlist[k,h] - Rlist[k,m]*Rlist[m,h]))
+  			Psi[[jj,kk]] <- 0.5*(term1 + term2 + term3 + term4)
 	}
 }
 
-sigmaLS <- Psi
+sigmaLS <- makesymmetric(Psi)
 
 gammaGLS <- solve(t(delta)%*%solve(sigmaLS)%*%delta)%*%(t(delta)%*%solve(sigmaLS)%*%(correlations - rhostar))
 
 rhoGLS <- delta%*%gammaGLS + rhostar
-
-print(rhoGLS)
 
 Rlist2 <- data
 rows <- nrow(hypothesis)
@@ -86,5 +86,8 @@ for (jj in 1:rows) {
 }
 
 X2 <- (N - 3)*t((z(correlations) - z(rhoGLS)))%*%solve(SLS)%*%(z(correlations) - z(rhoGLS))
+cat('X2=',X2)
 
-print(X2)
+
+p <- pchisq(X2, df=(k-q), lower.tail=FALSE)
+cat('p-value=',p)
