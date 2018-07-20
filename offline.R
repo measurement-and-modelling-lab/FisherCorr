71e62ab6302ebeb2388a4cf41a836bb032eefceb
paulserafini <pserafin@sfu.ca>
2018-07-19 20:22:28 -0700

    Uncomment offline version's input code

--- offline.R
+++ offline.R
@@ -1,109 +1,93 @@
 ## ## Load functions
-## compute4thOrderMoments <- dget("./wbcorr/compute4thOrderMoments.R")
-ztest <- dget("./multicorr/ztest.R")
-## errorcheck <- dget("./wbcorr/errorcheck.R")
+ComputeMulticorrChiSquare <- dget("./multicorr/ComputeMulticorrChiSquare.R")
 tablegen <- dget("./multicorr/tablegen.R")
 
 
-## ## Stipulate data type
-## cat("\nWhat type of data will you be using? 1. Raw data 2. Correlation data\n")
-## datatype <- readline(prompt="")
-## if (datatype == 1) {
-##     methods <- c("GLS", "TSGLS", "ADF", "TSADF")
-##     cat_methods <- "1. GLS  2. TSGLS  3. ADF  4. TSADF"
-##     datatype <- "rawdata"
-## } else if (datatype == 2) {
-##     methods <- c("GLS", "TSGLS")
-##     cat_methods <- "1. GLS  2. TSGLS"
-##     datatype <- "correlation"
-## } else {
-##     stop("Invalid data type.")
-## }
+## Stipulate data type
+cat("\nWhat type of data will you be using? 1. Raw data 2. Correlation data\n")
+datatype <- readline(prompt="")
+if (datatype == 1) {
+    methods <- c("GLS", "TSGLS", "ADF", "TSADF")
+    cat_methods <- "1. GLS  2. TSGLS  3. ADF  4. TSADF"
+    datatype <- "rawdata"
+} else if (datatype == 2) {
+    methods <- c("GLS", "TSGLS")
+    cat_methods <- "1. GLS  2. TSGLS"
+    datatype <- "correlation"
+} else {
+    stop("Invalid data type.")
+}
 
 
-## ## Specify input files
-## cat("\nInput your files in the following format: data.csv;hypothesis.csv\n")
-## files <- readline(prompt="")
-## files <- strsplit(files, ";")[[1]]
-## data.length <- length(files) - 1
-## if (length(files) != 1) {
-##     stop("Your files should consist of one data file and one hypothesis file.")
-## }
+## Specify input files
+cat("\nInput your files in the following format: data.csv;hypothesis.csv\n")
+files <- readline(prompt="")
+files <- strsplit(files, ";")[[1]]
+data.length <- length(files) - 1
+if (data.length != 1) {
+    stop("Your files should consist of one data file and one hypothesis file.")
+}
 
 
-## ## Read data file(s)
-## filename <- files[[1]]
-## file.exists <- file.exists(filename)
-## if (file.exists) {
-##     data <- read.csv(file=filename,head=FALSE, sep=",")
-##     data <- as.matrix(data)
-## } else {
-##     stop("Data file does not exist.")
-## }
+## Read data file(s)
+filename <- files[[1]]
+file.exists <- file.exists(filename)
+if (file.exists) {
+    data <- read.csv(file=filename,head=FALSE, sep=",")
+    data <- as.matrix(data)
+} else {
+    stop("Data file does not exist.")
+}
 
 
-## ## Read hypothesis file
-## hypothesis.file <- files[[2]]
-## file.exists <- file.exists(hypothesis.file)
-## if (file.exists) {
-##     hypothesis <- read.csv(file=hypothesis.file,head=FALSE)
-##     hypothesis <- as.matrix(hypothesis)
-## } else {
-##     stop("Hypothesis file does not exist")
-## }
+## Read hypothesis file
+hypothesis.file <- files[[2]]
+file.exists <- file.exists(hypothesis.file)
+if (file.exists) {
+    hypothesis <- read.csv(file=hypothesis.file,head=FALSE)
+    hypothesis <- as.matrix(hypothesis)
+} else {
+    stop("Hypothesis file does not exist")
+}
 
 
-## ## Generate sample size list
-## if (datatype == "rawdata") {
-##     N <- nrow(data)
-## } else {
-##     cat("\nInput the sample size for your data:\n")
-##     N <- readline(prompt="")
-## }
+## Generate sample size list
+if (datatype == "rawdata") {
+    N <- nrow(data)
+} else {
+    cat("\nInput the sample size for your data:\n")
+    N <- readline(prompt="")
+}
 
 
-## ## Select estimation methods to be included
-## cat("\nWhat estimation method would you like to use?", cat_methods, "\n")
-## method.index <- readline(prompt="")
-## if (method.index %in% 1:length(methods)) {
-##     method.index <- as.numeric(method.index)
-##     estimation.method <- methods[method.index]
-## } else {
-##     stop("Invalid estimation method.")
-## }
+## Select estimation methods to be included
+cat("\nWhat estimation method would you like to use?", cat_methods, "\n")
+method.index <- readline(prompt="")
+if (method.index %in% 1:length(methods)) {
+    method.index <- as.numeric(method.index)
+    estimation.method <- methods[method.index]
+} else {
+    stop("Invalid estimation method.")
+}
 
 
-## ## Choose how to deal with missing data
-## if (datatype == "rawdata") {
-##     cat("\nHow would you like to deal with missing data? 1. Listwise  2. Pairwise  3. There is no missing data\n")
-##     deletion.index <- readline(prompt="")
-##     if (!(deletion.index %in% c("1", "2", "3"))) {
-##         stop("Invalid deletion method.")
-##     } else {
-##         deletion.index <- as.numeric(deletion.index)
-##         deletion <- c("listwise", "pairwise", "nodeletion")[deletion.index]
-##     }
-## } else {
-##     deletion <- "nodeletion"
-## }
+## Choose how to deal with missing data
+if (datatype == "rawdata") {
+    cat("\nHow would you like to deal with missing data? 1. Listwise  2. Pairwise  3. There is no missing data\n")
+    deletion.index <- readline(prompt="")
+    if (!(deletion.index %in% c("1", "2", "3"))) {
+        stop("Invalid deletion method.")
+    } else {
+        deletion.index <- as.numeric(deletion.index)
+        deletion <- c("listwise", "pairwise", "nodeletion")[deletion.index]
+    }
+} else {
+    deletion <- "nodeletion"
+}
 
-
-
-
-data <- read.csv(file="example1_data1.csv",head=FALSE, sep=",")
-data <- as.matrix(data)
-
-hypothesis <- read.csv(file="example1_hypothesis.csv",head=FALSE, sep=",")
-hypothesis <- as.matrix(hypothesis)
-
-N <- nrow(data)
-hypothesis <- matrix(c(1,1,1,2,3,3,1,1,2,1,1,1,0,0,0), nrow=3)
-datatype <- "rawdata"
-estimation.method <- "TSADF"
-deletion <- "nodeletion"
 
 ## Run the test
-output <- ztest(data, N, hypothesis, datatype, estimation.method, deletion)
+output <- ComputeMulticorrChiSquare(data, N, hypothesis, datatype, estimation.method, deletion)
 
 
 ## Print the original hypothesis matrix
